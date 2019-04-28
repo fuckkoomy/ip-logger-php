38b17cd6a81fe7d113743c9c68758df1252ece0c
koomy <48370482+koomyuwu@users.noreply.github.com>
2019-04-28 02:31:45 -0300

    u
    u

---
+++ logger.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$ip = $_SERVER ('REMOTE_ADDR');
+
+
+
+
+$file = "uwu.txt"
+$text = file_get_contents ($file); 
+$text .= . "\n";  
+file_put_contents ($file, $text); 
+
+ 
+
+
+
+
+
+
+
+?>
+?>
